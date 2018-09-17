804a2e104c0da06b9b5c4c0ef6793984886c436a
BMSalamander <hostan3@gmail.com>
2018-09-17 13:58:03 +0500

    Антипьев Александр. Задание 11. Исправлено

--- task11/src/com/example/task11/Task11.java
+++ task11/src/com/example/task11/Task11.java
@@ -1,19 +1,13 @@
 package com.example.task11;
 
+import java.lang.StrictMath.*;
+
 public class Task11 {
 
     public static float benefit(float sum, float percent) {
 
         // TODO исправьте функцию, чтобы избежать накопления ошибки
-        // Считаем проценты за год
-        if (percent > 0.001) {
-            for (int i = 0; i < 12; ++i) {
-                sum += sum * percent;
-            }
-        }
-        else {
-            sum += sum * (percent * 12);
-        }
+        sum = (float)((double)sum * StrictMath.pow(1d + percent, 12d));
 
         return sum;
     }
@@ -23,7 +17,7 @@ public class Task11 {
         float sum = 500; // 500 руб. на счете
         float percent = 0.00000001f; // 0.000001% ежемесячно
 
-        sum = benefit(sum, percent);
+        sum = benefit(sum, percent); //563.46405f, 500.00006f
 
         System.out.println("Сумма на счете через год: " + sum);
 
